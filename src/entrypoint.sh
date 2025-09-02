b6bb6eaee74850a1f8fabade4a488b116f7c3eed
IThundxr <me@ithundxr.dev>
2025-09-02 10:41:13 -0400

    improve image

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
+set -e
 
-if ! grep -q $SSH_USERNAME /etc/group &> /dev/null; then
+if ! getent group "$SSH_USERNAME" > /dev/null 2>&1; then
     echo "Creating group..."
-    addgroup $SSH_USERNAME
+    groupadd "$SSH_USERNAME"
 fi
 
-if ! id $SSH_USERNAME &> /dev/null; then
+if ! id "$SSH_USERNAME" > /dev/null 2>&1; then
     echo "Creating user..."
-    adduser --home $HOME_FOLDER --ingroup $SSH_USERNAME $SSH_USERNAME
+    useradd \
+        --create-home \
+        --home-dir "$HOME_FOLDER" \
+        --gid "$SSH_USERNAME" \
+        --shell /bin/bash \
+        "$SSH_USERNAME"
 fi
 
 echo "Setting password..."
-echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" | passwd $SSH_USERNAME
+echo "$SSH_USERNAME:$SSH_PASSWORD" | chpasswd
 
 echo "Setting home folder permissions..."
-chown -R $SSH_USERNAME:$SSH_USERNAME $HOME_FOLDER
+chown -R "$SSH_USERNAME:$SSH_USERNAME" "$HOME_FOLDER"
 
-if [ -f $SETUP_SCRIPT_PATH ]; then
+if [ -f "$SETUP_SCRIPT_PATH" ]; then
     echo "Running setup script..."
-    bash $SETUP_SCRIPT_PATH
+    bash "$SETUP_SCRIPT_PATH"
     echo "Finished running setup script..."
 fi
 
